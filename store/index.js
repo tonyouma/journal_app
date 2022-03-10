b4413c94b1330ff98d220df01242f95f474b86e1
TonyOuma <kyrietony@gmail.com>
2022-03-10 12:01:58 +0300

    feat: initial store setup

---
+++ store/index.js
@@ -0,0 +1,16 @@
+import VueX from 'vuex'
+import Vue from 'vue'
+
+
+// Loading VueX
+Vue.use(VueX)
+
+/**
+ * create a store and export it is a default method
+ * that returns the VueX store instance   */
+
+// add journals state to the store
+
+export default () => new VueX.Store({
+
+})
