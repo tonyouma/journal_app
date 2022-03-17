6e4c698a8b9480df742c1cfae8ae2e8904ac725e
TonyOuma <kyrietony@gmail.com>
2022-03-17 12:11:45 +0300

    feat: add icon components

---
+++ components/Icons/Cloud/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <cloud-icon size="1.3x" class="text-gray-400"></cloud-icon>
+</template>
+
+<script>
+import { CloudIcon } from 'vue-feather-icons'
+
+export default {
+  components: {
+    CloudIcon,
+  },
+}
+</script>
---
+++ components/Icons/Trash/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <trash-2-icon size="1.3x" class="text-red-600"></trash-2-icon>
+</template>
+
+<script>
+import { Trash2Icon } from 'vue-feather-icons'
+
+export default {
+  components: {
+    Trash2Icon,
+  },
+}
+</script>
